086b5e5498a211d33bb147dc3caaf0a91893a36c
Ntebaleng <you@example.com>
2022-09-08 11:35:46 -0700

    d

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  *
  * * main - prints out a string via printf
@@ -9,17 +6,14 @@
  * * Description: Program that prints out the string s followed by a new line
  *
  * * Return: int 0 when done and no errors
- *
- * */
-
+ */
 
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 
 }
