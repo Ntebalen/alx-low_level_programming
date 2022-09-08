2b6640fc013000667552e4726a8a64cb70261e56
Ntebaleng <you@example.com>
2022-09-08 11:47:08 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 /**
- *
- * * main - prints
- *
- * * Description: 
- *
- * * Return: int 0 when done and no errors
- *
- * */
+ * main - prints
+ * Description: 
+ * Return: int 0 when done and no errors
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+return (0);
 
 }
