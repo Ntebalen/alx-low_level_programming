8bd0ed35f558f80e7a7b11f97cfdde3657870c5f
Ntebaleng <you@example.com>
2022-09-13 11:35:46 -0700

    fd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,43 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *sh = "Main";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+
+
+/**
+
+ * main - print putchar
+
+ * Return: always 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
 	_putchar('\n');
 
+
+
 	return (0);
+
+
+
 }
